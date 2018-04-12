bca049a455e46f383d73bcd8061e0e4bad9689db
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-12 22:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(){
     return `There is nobody waiting to be served!`;
   }
   else{
-    console.log(`Currently serving ${katzDeliLine[0]}.`);
+    (`Currently serving ${katzDeliLine[0]}.`);
     katzDeliLine.shift();
   }
 }
