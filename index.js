5f8364b4c16140329c9ed7b1c1932a715b68f8dc
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-12 22:33:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var katzDeliLine = [];
+var katzDeli = [];
 //katzDeliLine is an array that contains current list of people
 //name is the new person's name to add to the list
 function takeANumber(katzDeliLine,name){
