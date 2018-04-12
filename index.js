ccff01a1e104371fba9f4a026ef4ee03b207278e
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-12 23:11:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ function nowServing(katzDeliLine){
   
 }
 
-function currentLine(){
-  
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0){
+    return `The line is currently empty.`;
+  }
+  else{
+    
+  }
+  return `The line is currently: 
    
 }
